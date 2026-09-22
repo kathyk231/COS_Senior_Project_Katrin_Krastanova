7da3930e725126598909cbfe7b289549d308f89a
kathyk231 <kkk231@aubg.edu>
2026-09-22 12:50:00 +0300

    Created a very basic window of the game

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("This is a test to see if GitHub is working, please ignore")
-print("Test Final")
+import pygame
+
+WIDTH, HEIGHT, FPS = 960, 540, 60
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Window Test One")
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((30,35,50))
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()

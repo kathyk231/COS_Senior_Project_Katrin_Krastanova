4af915324261a97e8ad3d5983b84e2522b7f7aec
kathyk231 <kkk231@aubg.edu>
2026-09-22 13:30:26 +0300

    Cleaned bugs + successful display of a "character"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+from player import Player
 
 WIDTH, HEIGHT, FPS = 960, 540, 60
-
+player = Player((400,300))
 def main():
     pygame.init()
 
@@ -10,10 +11,13 @@ def main():
     clock = pygame.time.Clock()
     running = True
     while running:
+        dt = clock.tick(60)/1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        player.update(dt)
         screen.fill((30,35,50))
+        player.draw(screen)
         pygame.display.flip()
     pygame.quit()
 
--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player:
         self.speed = 170
         self.radius = 7
 
-    def update(self, dt, navmesh):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
         direction = pygame.Vector2(0, 0)
         if keys[pygame.K_w] or keys[pygame.K_UP]:
@@ -14,14 +14,14 @@ class Player:
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             direction.y += 1
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            direction.x += 1
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             direction.x -= 1
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            direction.x += 1
         if direction.length_squared() == 0:
             return
         direction = direction.normalize()
-        new_position = (self.position+direction*self.speed*dt)
-        if navmesh.is_walkable(new_position):
-            self.position = new_position
+        new_position = (self.position + direction * self.speed * dt)
+        #if navmesh.is_walkable(new_position):
+        #    self.position = new_position
     def draw(self, surface):
         pygame.draw.circle(surface, (193, 217, 150), (int(self.position.x), int(self.position.y)), self.radius)
